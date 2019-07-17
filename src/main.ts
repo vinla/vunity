3bf6cc12bb619439e48eaf9b0e11afbb69e262c9
Vincent Crowe <vincent.crowe@emishealth.com>
2019-07-17 10:29:34 +0100

    Added simple animator

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as Vunity from './vunity';
 import InputManager from './input-manager';
-import { EllipseRenderer, BoxRenderer } from './renderers';
-import { CircleCollider, BoxCollider } from './colliders';
+import { Animation, BoxRenderer, AnimationFrame, AnimatedSpriteRender } from './renderers';
+import { BoxCollider } from './colliders';
 import { BasicController } from './custom';
 
 InputManager.init();
@@ -24,5 +24,36 @@ for ( let i = 0; i < 6; i++ ) {
     block.collider = new BoxCollider(block, {width: 32, height: 32});
     scene.addObject(block);
 }
+
+const cat = new Vunity.GameObject({x: 128, y: 256});
+const spriteSheet = new Image();
+spriteSheet.src = '/images/runningcat.png';
+
+const runningAnimation = {name: 'run', frames: [] as AnimationFrame[]};
+runningAnimation.frames.push({cellIndex: 0, frames: 4});
+runningAnimation.frames.push({cellIndex: 1, frames: 4});
+runningAnimation.frames.push({cellIndex: 2, frames: 4});
+runningAnimation.frames.push({cellIndex: 3, frames: 4});
+runningAnimation.frames.push({cellIndex: 4, frames: 4});
+runningAnimation.frames.push({cellIndex: 5, frames: 4});
+runningAnimation.frames.push({cellIndex: 6, frames: 4});
+runningAnimation.frames.push({cellIndex: 7, frames: 4});
+
+const animator = new AnimatedSpriteRender(
+    spriteSheet, [
+        {x: 0, y: 0, width: 512, height: 256},
+        {x: 512, y: 0, width: 512, height: 256},
+        {x: 0, y: 256, width: 512, height: 256},
+        {x: 512, y: 256, width: 512, height: 256},
+        {x: 0, y: 512, width: 512, height: 256},
+        {x: 512, y: 512, width: 512, height: 256},
+        {x: 0, y: 768, width: 512, height: 256},
+        {x: 512, y: 768, width: 512, height: 256},
+    ]);
+
+animator.addAnimation(runningAnimation);
+animator.startAnimation('run');
+cat.addComponent(animator);
+scene.addObject(cat);
 
 scene.run();
--- src/renderers.ts
+++ src/renderers.ts
@@ -1,4 +1,4 @@
-import { GameObjectComponent } from './vunity';
+import { GameObjectComponent, GameObject } from './vunity';
 import { Rect2D, Point2D, Size2D } from './utility';
 
 export class SpriteRenderer implements GameObjectComponent {
@@ -30,6 +30,83 @@ export class SpriteRenderer implements GameObjectComponent {
             this.sourceRect.width,
             this.sourceRect.height);
     }
+}
+
+export class AnimatedSpriteRender implements GameObjectComponent {
+    shouldRender: true;
+    enabled: true;
+    parent: GameObject;
+
+    image: CanvasImageSource;
+    cells: Rect2D[];
+    animations: Animation[];
+
+    currentAnimation: Animation;
+    currentIndex: number;
+    framesLeft: number;
+
+    constructor(image: CanvasImageSource, cells: Rect2D[]) {
+        this.image = image;
+        this.cells = cells;
+        this.animations = [] as Animation[];
+    }
+
+    addAnimation = (animation: Animation) => {
+        this.animations.push(animation);
+    }
+
+    startAnimation = (name: string) => {
+        const match = this.animations.filter(anim => anim.name === name);
+        if (match.length === 0)
+            return;
+        this.currentAnimation = match[0];
+        this.currentIndex = 0;
+        this.framesLeft = this.currentAnimation.frames[0].frames;
+    }
+
+    update = (elapsed: number) => {
+        if (this.animations.length < 1)
+            return;
+
+        if (this.currentIndex < 0)
+            return;
+
+        this.framesLeft--;
+
+        if (this.framesLeft < 0) {
+            this.currentIndex = (this.currentIndex + 1) % this.currentAnimation.frames.length;
+            this.framesLeft = this.currentAnimation.frames[this.currentIndex].frames;
+        }
+    }
+
+    draw = (drawingContext: CanvasRenderingContext2D, offset: Point2D) => {
+        if (this.animations.length < 1)
+            return;
+
+        const frame = this.currentAnimation.frames[this.currentIndex];
+        const sourceRect = this.cells[frame.cellIndex];
+
+        drawingContext.drawImage(
+            this.image,
+            sourceRect.x,
+            sourceRect.y,
+            sourceRect.width,
+            sourceRect.height,
+            offset.x,
+            offset.y,
+            sourceRect.width,
+            sourceRect.height);
+    }
+}
+
+export interface Animation {
+    name: string;
+    frames: AnimationFrame[];
+}
+
+export interface AnimationFrame {
+    cellIndex: number;
+    frames: number;
 }
 
 export class BoxRenderer implements GameObjectComponent {
